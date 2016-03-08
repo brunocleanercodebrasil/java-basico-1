1a4ac9432173f7734004bbe725759491c38b5daf
Bruno da Silva Dias <silvadias181@gmail.com>
2016-03-07 23:19:31 -0300

    Update AskOneNumber.java

--- AskOneNumber.java
+++ AskOneNumber.java
@@ -9,10 +9,10 @@ public class AskOneNumber {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Digite um n�mero");
+		System.out.println("Digite um número");
 		int num = scan.nextInt();
 		
-		System.out.println("O n�mero dogitado foi "+ num );
+		System.out.println("O número dogitado foi "+ num );
 	}
 
 }
